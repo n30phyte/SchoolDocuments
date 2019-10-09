adfdd7c11ed36e6d31f43460b2d2e15e5c12f020
Michael Kwok <michael.kwok00@gmail.com>
2019-10-09 17:42:46 -0600

    Maybe working?

--- Assignment/Huffman/huffman.py
+++ Assignment/Huffman/huffman.py
@@ -3,16 +3,38 @@ class TreeLeaf:
     Leaf node of a Huffman tree. Stores the value.
 
     Should store an 8-bit integer to represent a single byte, or None
-    to indicate the special "end of message" character.
+    to indicate the special "end of file" character.
     """
-    pass
+
+    def __init__(self, uncompressed_byte):
+        self.__value = uncompressed_byte
+
+    def getValue(self):
+        return self.__value
+
+    def __str__(self):
+        return "Leaf storing " + str(self.__value)
+
 
 class TreeBranch:
     """
-    Simple representation of an internal node on a Huffman tree.
+    Simple representation of a subtree/tree of a Huffman tree.
     Just stores the two children.
     """
-    pass
+
+    def __init__(self, lchild, rchild):
+        self.__left = lchild
+        self.__right = rchild
+
+    def getLeft(self):
+        return self.__left
+
+    def getRight(self):
+        return self.__right
+
+    def __str__(self):
+        return "(" + str(self.__left) + " <- branch root -> " + str(self.__right) + ")"
+
 
 def custom_min(trees):
     """ Takes a list of tuples called trees, finds the smallest 
@@ -21,22 +43,70 @@ def custom_min(trees):
 
     Each item in trees is a tuple of (symbol, frequency)
     """
-    pass
 
+    if len(trees) == 0:
+        raise ValueError("The list passed as input was empty.")
+
+    # default to the first item
+    min_item = trees[0]
+    min_index = 0
+
+    for i in range(len(trees)):
+        # if this item has a smaller frequency
+        if trees[i][1] < min_item[1]:
+            min_item = trees[i]
+            min_index = i
+
+    del trees[min_index]
+
+    return min_item[0], min_item[1], trees
 
 
 def make_tree(freq_table):
     """
     Constructs and returns the Huffman tree from the given frequency table.
     """
-    pass
+
+    trees = []
+    trees.append((TreeLeaf(None), 1))
+
+    for (symbol, freq) in freq_table.items():
+        trees.append((TreeLeaf(symbol), freq))
+
+    while len(trees) > 1:
+        right, rfreq, trees = custom_min(trees)
+        left, lfreq, trees = custom_min(trees)
+        trees.append((TreeBranch(left, right), lfreq+rfreq))
+
+    return trees[0][0]
+
 
 def make_encoding_table(huffman_tree):
     """
     Given a Huffman tree, will make the encoding table mapping each
     byte (leaf node) to its corresponding bit sequence in the tree.
     """
-    pass
+    encoding_table = {}
+    preorder(huffman_tree, encoding_table, ())
+    return encoding_table
+
+
+def preorder(tree, table, path):
+    """
+    Traces out all paths in the Huffman tree and adds each
+    corresponding leaf value and its associated path to the table.
+    """
+    if isinstance(tree, TreeLeaf):  # base case
+        # note, if this is the special end file entry then
+        # it stores table[None] = path
+        table[tree.getValue()] = path
+    elif isinstance(tree, TreeBranch):
+        # the trailing comma (,) means this is properly interpreted
+        # as a tuple and not just a single boolean value
+        preorder(tree.getLeft(), table, path + (False, ))
+        preorder(tree.getRight(), table, path + (True, ))
+    else:
+        raise TypeError('{} is not a tree type'.format(type(tree)))
 
 
 def make_freq_table(stream):
@@ -46,4 +116,21 @@ def make_freq_table(stream):
 
     The frequency table is actually a dictionary.
     """
-    pass
+    freq_dict = {}
+    buff = bytearray(512)
+    end_of_stream = False
+    while not end_of_stream:
+        # read bytes into a pre-allocated bytes-like object (bytearray)
+        # and return number of bytes read
+        count = stream.readinto(buff)
+
+        for single_byte in buff:
+            if single_byte not in freq_dict:
+                freq_dict[single_byte] = 1
+            else:
+                freq_dict[single_byte] += 1
+
+        if count < len(buff):  # end of stream
+            end_of_stream = True
+
+    return freq_dict
--- Assignment/Huffman/util.py
+++ Assignment/Huffman/util.py
@@ -4,7 +4,7 @@ import pickle
 
 
 def read_tree(tree_stream):
-    '''Read a description of a Huffman tree from the given compressed
+    """Read a description of a Huffman tree from the given compressed
     tree stream, and use the pickle module to construct the tree object.
     Then, return the root node of the tree itself.
 
@@ -13,8 +13,9 @@ def read_tree(tree_stream):
 
     Returns:
       A Huffman tree root constructed according to the given description.
-    '''
-    pass
+    """
+    return pickle.load(tree_stream)
+
 
 def decode_byte(tree, bitreader):
     """
@@ -29,11 +30,21 @@ def decode_byte(tree, bitreader):
     Returns:
       Next byte of the compressed bit stream.
     """
-    pass
+
+    while isinstance(tree, huffman.TreeBranch):
+      try:
+        if bitreader.readbit():
+            tree = tree.getRight()
+        else:
+            tree = tree.getLeft()
+      except EOFError:
+        return None
+
+    return tree.getValue()
 
 
 def decompress(compressed, uncompressed):
-    '''First, read a Huffman tree from the 'compressed' stream using your
+    """First, read a Huffman tree from the 'compressed' stream using your
     read_tree function. Then use that tree to decode the rest of the
     stream and write the resulting symbols to the 'uncompressed'
     stream.
@@ -42,21 +53,33 @@ def decompress(compressed, uncompressed):
       compressed: A file stream from which compressed input is read.
       uncompressed: A writable file stream to which the uncompressed
           output is written.
-    '''
-    pass
+    """
+
+    huffman_root = read_tree(compressed)
+    filestream = bitio.BitReader(compressed)
+
+    output = bytearray()
+
+    for decoded in iter(lambda: decode_byte(huffman_root, filestream), None):
+        output.append(decoded)
+
+    uncompressed.write(output)
+
 
 def write_tree(tree, tree_stream):
-    '''Write the specified Huffman tree to the given tree_stream
+    """Write the specified Huffman tree to the given tree_stream
     using pickle.
 
     Args:
       tree: A Huffman tree.
       tree_stream: The binary file to write the tree to.
-    '''
-    pass
+    """
+
+    pickle.dump(tree, tree_stream)
+
 
 def compress(tree, uncompressed, compressed):
-    '''First write the given tree to the stream 'compressed' using the
+    """First write the given tree to the stream 'compressed' using the
     write_tree function. Then use the same tree to encode the data
     from the input stream 'uncompressed' and write it to 'compressed'.
     If there are any partially-written bytes remaining at the end,
@@ -69,5 +92,26 @@ def compress(tree, uncompressed, compressed):
       uncompressed: A file stream from which you can read the input.
       compressed: A file stream that will receive the tree description
           and the coded input data.
-    '''
-    pass
+    """
+
+    write_tree(tree, compressed)
+
+    encoding_table = huffman.make_encoding_table(tree)
+
+    instream = bitio.BitReader(uncompressed)
+    outstream = bitio.BitWriter(compressed)
+
+    EOF = False
+    while not EOF:
+        try:
+            byte = instream.readbits(8)
+            compressed_byte = encoding_table[byte]
+            for bit in compressed_byte:
+                outstream.writebit(int(bit))
+        except EOFError:
+            EOF = True
+    #
+    # padding_length = (bitsWritten % 8)
+    # outstream.writebits(0, padding_length)
+    #
+    # outstream.flush()
