c6984c20d90792f5ae1894a4587fbe838e198ed0
Michael Kwok <michael.kwok00@gmail.com>
2019-10-23 23:34:05 -0600

    Fixed

--- WeeklyExercise/counting_lights/counting.cpp
+++ WeeklyExercise/counting_lights/counting.cpp
@@ -13,10 +13,10 @@ const void output_pins(const unsigned int* pin_array,
                        const int array_length,
                        const int value) {
   for (auto i = 0; i < array_length; i++) {
-    auto output_signal = (value & (1 << i)) ? HIGH : LOW;
-
-    Serial.println(value & (1 << i));
-
+    int output_signal = LOW;
+    if ((value & (1 << i)) != 0) {
+      output_signal = HIGH;
+    }
     digitalWrite(LED_PINS[i], output_signal);
   }
 }
@@ -27,13 +27,13 @@ void setup() {
   Serial.begin(9600);
 
   // Set pin modes
-  pinMode(STORE_LOAD_PIN, INPUT);
+  pinMode(STORE_LOAD_PIN, INPUT_PULLUP);
 
-  pinMode(DECREMENT_PIN, INPUT);
-  pinMode(INCREMENT_PIN, INPUT);
+  pinMode(DECREMENT_PIN, INPUT_PULLUP);
+  pinMode(INCREMENT_PIN, INPUT_PULLUP);
 
   for (auto i = 0; i < LED_COUNT; i++) {
-    pinMode(INCREMENT_PIN, OUTPUT);
+    pinMode(LED_PINS[i], OUTPUT);
   }
 
   // Reset output
@@ -43,58 +43,57 @@ void setup() {
 int main() {
   setup();
 
-  unsigned char counter = 0;
-
   auto decrement_old = digitalRead(DECREMENT_PIN);
   auto increment_old = digitalRead(INCREMENT_PIN);
 
   auto store_old = digitalRead(STORE_LOAD_PIN);
 
+  unsigned char counter = 0;
   unsigned char memory = 0;
+
   bool stored = false;
 
   while (true) {
-    // Increment/Decrement
+    // Read inputs
     auto decrement_new = digitalRead(DECREMENT_PIN);
     auto increment_new = digitalRead(INCREMENT_PIN);
+
+    auto store_new = digitalRead(STORE_LOAD_PIN);
 
     // Only fire when new signal and old signal different.
     if (decrement_new != decrement_old || increment_new != increment_old) {
       // Increment or decrement
-      if (increment_new == HIGH) {
+      if (increment_new == LOW) {
         counter++;
-      } else if (decrement_new == HIGH) {
+      } else if (decrement_new == LOW) {
         --counter;
       }
-      Serial.println(counter);
+
       decrement_old = decrement_new;
       increment_old = increment_new;
-    }
 
-    // If counter overflows backwards, fix it
-    if (counter == (1 << LED_COUNT)) {
-      counter = (1 << LED_COUNT) - 1;
-      Serial.println(counter);
-    }
-
-    // Store/Load
-    auto store_new = digitalRead(STORE_LOAD_PIN);
-
-    if (store_new != store_old) {
-      if (store_new == HIGH) {
+    } else if (store_new != store_old) {
+      if (store_new == LOW) {
         if (stored) {
           counter = memory;
           stored = false;
+          Serial.println("Restoring");
         } else {
+          Serial.println("Storing");
           memory = counter;
           counter = 0;
           stored = true;
         }
       }
+      store_old = store_new;
     }
 
+    // If counter overflows backwards, fix it
+    if (counter == 32) {
+      counter = 0;
+    } else if (counter == 255) {
+      counter = 31;
+    }
     output_pins(LED_PINS, LED_COUNT, counter);
   }
-
-  return 0;
 }
