af0f99fd86f831bef592a9f7e26a07341b69de51
Michael Kwok <michael.kwok00@gmail.com>
2019-10-28 17:49:32 -0600

    Added cpp properties file

--- WeeklyExercise/counting_lights/counting.cpp
+++ WeeklyExercise/counting_lights/counting.cpp
@@ -24,48 +24,36 @@ char LED_output[LED_COUNT] = {LOW, LOW, LOW, LOW, LOW};
  * Subroutine to output the stored state of the LEDs
  */
 const void output_pins() {
-  for (auto i = 0; i < LED_COUNT; i++) {
+  for (int i = 0; i < LED_COUNT; i++) {
     digitalWrite(LED_PINS[i], LED_output[i]);
   }
 }
 
 /*
  * Increments the stored array by one
- *
- * @param index: the current index of the array being looked at.
  */
-void increment(unsigned int index = 0) {
-  switch (LED_output[index]) {
-    case HIGH:
-      LED_output[index] = LOW;
-      // recursively increments the array while LED is not off.
-      if (index < (LED_COUNT - 1)) {
-        increment(index + 1);
-      }
-      break;
-    case LOW:
-      LED_output[index] = HIGH;
-      break;
+void increment() {
+  unsigned int index = 0;
+  while ((LED_output[index] != LOW) && (index < LED_COUNT)) {
+    LED_output[index] = LOW;
+    index++;
+  }
+  if (LED_output[index] == LOW) {
+    LED_output[index] = HIGH;
   }
 }
 
 /*
  * Decrements the stored array by one
- *
- * @param index: the current index of the array being looked at.
  */
-void decrement(unsigned int index = 0) {
-  switch (LED_output[index]) {
-    case HIGH:
-      LED_output[index] = LOW;
-      break;
-    case LOW:
-      // recursively decrements the array while LED is not on.
-      LED_output[index] = HIGH;
-      if (index < (LED_COUNT - 1)) {
-        decrement(index + 1);
-      }
-      break;
+void decrement() {
+  unsigned int index = 0;
+  while ((LED_output[index] != HIGH) && (index < LED_COUNT)) {
+    LED_output[index] = HIGH;
+    index++;
+  }
+  if (LED_output[index] == HIGH) {
+    LED_output[index] = LOW;
   }
 }
 
@@ -75,7 +63,7 @@ void decrement(unsigned int index = 0) {
  * @param input: the array where the LED output is stored
  */
 void store(char* input) {
-  for (auto i = 0; i < LED_COUNT; i++) {
+  for (int i = 0; i < LED_COUNT; i++) {
     *(input + i) = *(LED_output + i);
     *(LED_output + i) = LOW;
   }
@@ -87,7 +75,7 @@ void store(char* input) {
  * @param input: the array where the LED output was stored
  */
 void load(const char* input) {
-  for (auto i = 0; i < LED_COUNT; i++) {
+  for (int i = 0; i < LED_COUNT; i++) {
     *(LED_output + i) = *(input + i);
   }
 }
@@ -111,7 +99,7 @@ void setup() {
   pinMode(INCREMENT_PIN, INPUT_PULLUP);
 
   // Set LED pin modes
-  for (auto i = 0; i < LED_COUNT; i++) {
+  for (int i = 0; i < LED_COUNT; i++) {
     pinMode(LED_PINS[i], OUTPUT);
   }
 
@@ -126,9 +114,9 @@ int main() {
   setup();
 
   // Assume all buttons are off at the start.
-  auto decrement_old = HIGH;
-  auto increment_old = HIGH;
-  auto store_old = HIGH;
+  int decrement_old = HIGH;
+  int increment_old = HIGH;
+  int store_old = HIGH;
 
   // Flag to remember if any data is stored.
   bool is_stored = false;
@@ -138,9 +126,9 @@ int main() {
 
   while (true) {
     // Read inputs
-    auto decrement_new = digitalRead(DECREMENT_PIN);
-    auto increment_new = digitalRead(INCREMENT_PIN);
-    auto store_new = digitalRead(STORE_LOAD_PIN);
+    int decrement_new = digitalRead(DECREMENT_PIN);
+    int increment_new = digitalRead(INCREMENT_PIN);
+    int store_new = digitalRead(STORE_LOAD_PIN);
 
     // Only fire when new signal and old signal different.
     if (decrement_new != decrement_old || increment_new != increment_old ||
