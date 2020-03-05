db280d9434220c491ca481a1f154ecfeab18defd
Michael Kwok <michael.kwok00@gmail.com>
2020-03-04 19:13:40 -0700

    Finished Weekly 6

--- WeeklyExercise/stl_concepts/complex_postfix.cpp
+++ WeeklyExercise/stl_concepts/complex_postfix.cpp
@@ -1,6 +1,62 @@
 #include <iostream>
+#include <utility>
+#include <stack>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::stack<std::pair<long long, long long>> OperationStack;
+    std::string input;
+
+    bool done = false;
+
+    while(!done) {
+        std::cin >> input;
+
+        if (input == "V") {
+            long long real;
+            long long complex;
+
+            std::cin >> real;
+            std::cin >> complex;
+
+            OperationStack.push(std::make_pair(real, complex));
+        } else if (input == "B") {
+
+            auto right = OperationStack.top();
+            OperationStack.pop();
+
+            auto left = OperationStack.top();
+            OperationStack.pop();
+
+            std::cin >> input;
+
+            if (input == "+") {
+                OperationStack.push(std::make_pair(left.first + right.first, left.second + right.second));
+            } else if (input == "-") {
+                OperationStack.push(std::make_pair(left.first - right.first, left.second - right.second));
+            } else if (input == "*") {
+                OperationStack.push(std::make_pair((left.first * right.first) - (left.second * right.second),
+                                                   (left.first * right.second) + (left.second * right.first)));
+            }
+        } else if (input == "U") {
+            auto target = OperationStack.top();
+            OperationStack.pop();
+
+            std::cin >> input;
+
+            if (input == "-") {
+                OperationStack.push(std::make_pair(-target.first, -target.second));
+            } else if (input == "c") {
+                OperationStack.push(std::make_pair(target.first, -target.second));
+            }
+        } else if (input == "S") {
+            auto output = OperationStack.top();
+            std::cout << output.first << " " << output.second << std::endl;
+            done = true;
+        } else {
+            std::cout << "Error: Incorrect input." << std::endl;
+            std::cin.sync();
+        }
+    }
     return 0;
 }
--- WeeklyExercise/stl_concepts/student_hash.cpp
+++ WeeklyExercise/stl_concepts/student_hash.cpp
@@ -20,20 +20,53 @@ int main() {
 
         if (input == "I") {
             std::cin >> name >> ID >> grade;
+            auto res = table.insert(StudentRecord{name, ID, grade});
 
-            table.insert(StudentRecord{name,ID, grade});
+            if (!res.second) {
+                message << "Error: Cannot insert duplicate ID" << std::endl;
+            }
         } else if (input == "R") {
+            std::cin >> ID;
+            auto target = table.find(StudentRecord{"", ID, 0});
 
+            if (target != table.end()) {
+                table.erase(target);
+            } else {
+                message << "Error: Cannot remove non-existent ID" << std::endl;
+            }
         } else if (input == "Q") {
+            auto query = StudentRecord();
+            std::cin >> input;
+            if (input == "i") {
+                std::cin >> ID;
+                query.id = ID;
+            } else if (input == "n") {
+                std::cin >> name;
+                query.name = name;
+            } else if (input == "g") {
+                std::cin >> grade;
+                query.grade = grade;
+            }
+
+            for (const auto &it : table) {
+                if (it == query) {
+                    message << "Name: " << it.name << ", ID: " << it.id << ", Grade: " << it.grade << std::endl;
+                }
+            }
+
+            if(message.str().empty()) {
+                message << "Error: No matches found" << std::endl;
+            }
 
         } else if (input == "S") {
             stop = true;
         } else {
             message << "Error: Incorrect input." << std::endl;
+            std::cin.sync();
         }
 
         std::cout << message.str();
-        message.clear();
+        message.str(std::string());
     }
 
     return 0;
