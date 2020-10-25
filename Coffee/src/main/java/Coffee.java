9288c9828b1f3d05d40e946072032fcee9aff8a6
Michael Kwok <michael.kwok00@gmail.com>
2020-10-25 15:37:20 -0600

    Part 1 Coffee done

--- Coffee/src/main/java/Coffee.java
+++ Coffee/src/main/java/Coffee.java
@@ -3,10 +3,18 @@
  * Part 1: The {@code Coffee} class
  */
 public class Coffee implements Comparable<Coffee> {
-    private int strength; // The strength of the coffee
+    private final int strength; // The strength of the coffee
+
+    public Coffee(int coffeeStrength) {
+        this.strength = coffeeStrength;
+    }
 
     @Override
     public int compareTo(Coffee coffee) {
         return Integer.compare(this.strength, coffee.strength);
+    }
+
+    public int getStrength() {
+        return strength;
     }
 }
