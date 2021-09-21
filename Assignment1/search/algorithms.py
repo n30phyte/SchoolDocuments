447e03f00f73f3ccd7ead8986505e0fa9e92e66d
Michael Kwok <michael.kwok00@gmail.com>
2021-09-20 20:33:33 -0600

    Dijkstra pass 1

--- Assignment1/search/algorithms.py
+++ Assignment1/search/algorithms.py
@@ -1,20 +1,23 @@
+from typing import Dict, List
 import heapq
+
 
 class State:
     """
     Class to represent a state on grid-based pathfinding problems. The class contains two static variables:
     map_width and map_height containing the width and height of the map. Although these variables are properties
     of the map and not of the state, they are used to compute the hash value of the state, which is used
-    in the CLOSED list. 
+    in the CLOSED list.
 
     Each state has the values of x, y, g, h, and cost. The cost is used as the criterion for sorting the nodes
     in the OPEN list for both Dijkstra's algorithm and A*. For Dijkstra the cost should be the g-value, while
-    for A* the cost should be the f-value of the node. 
+    for A* the cost should be the f-value of the node.
     """
+
     map_width = 0
     map_height = 0
-    
-    def __init__(self, x, y):
+
+    def __init__(self, x: int, y: int):
         """
         Constructor - requires the values of x and y of the state. All the other variables are
         initialized with the value of 0.
@@ -24,121 +27,160 @@ class State:
         self._g = 0
         self._h = 0
         self._cost = 0
-        
+
     def __repr__(self):
         """
         This method is invoked when we call a print instruction with a state. It will print [x, y],
-        where x and y are the coordinates of the state on the map. 
+        where x and y are the coordinates of the state on the map.
         """
         state_str = "[" + str(self._x) + ", " + str(self._y) + "]"
         return state_str
-    
+
     def __lt__(self, other):
         """
         Less-than operator; used to sort the nodes in the OPEN list
         """
         return self._cost < other._cost
-    
+
     def state_hash(self):
         """
-        Given a state (x, y), this method returns the value of x * map_width + y. This is a perfect 
+        Given a state (x, y), this method returns the value of x * map_width + y. This is a perfect
         hash function for the problem (i.e., no two states will have the same hash value). This function
-        is used to implement the CLOSED list of the algorithms. 
+        is used to implement the CLOSED list of the algorithms.
         """
         return self._y * State.map_width + self._x
-    
+
     def __eq__(self, other):
         """
         Method that is invoked if we use the operator == for states. It returns True if self and other
-        represent the same state; it returns False otherwise. 
+        represent the same state; it returns False otherwise.
         """
         return self._x == other._x and self._y == other._y
 
-    def get_x(self):
+    def get_x(self) -> int:
         """
         Returns the x coordinate of the state
         """
         return self._x
-    
-    def get_y(self):
+
+    def get_y(self) -> int:
         """
         Returns the y coordinate of the state
         """
         return self._y
-    
-    def get_g(self):
+
+    def get_g(self) -> float:
         """
         Returns the g-value of the state
         """
         return self._g
-        
-    def get_h(self):
+
+    def get_h(self) -> float:
         """
         Returns the h-value of the state
         """
         return self._h
-    
-    def get_cost(self):
+
+    def get_cost(self) -> float:
         """
         Returns the cost of the state (g for Dijkstra's and f for A*)
         """
         return self._cost
-    
-    def set_g(self, cost):
+
+    def set_g(self, cost: float):
         """
         Sets the g-value of the state
         """
         self._g = cost
-    
-    def set_h(self, h):
+
+    def set_h(self, h: float):
         """
         Sets the h-value of the state
         """
         self._h = h
-    
-    def set_cost(self, cost):
+
+    def set_cost(self, cost: float):
         """
         Sets the cost of a state (g for Dijkstra's and f for A*)
         """
         self._cost = cost
+
 
 class Search:
     """
     Interface for a search algorithm. It contains an OPEN list and a CLOSED list.
 
     The OPEN list is implemented with a heap, which can be done with the library heapq
-    (https://docs.python.org/3/library/heapq.html).    
-    
+    (https://docs.python.org/3/library/heapq.html).
+
     The CLOSED list is implemented as a dictionary where the state hash value is used as key.
     """
-    def __init__(self, gridded_map):
+
+    from search.map import Map
+
+    def __init__(self, gridded_map: Map):
         self.map = gridded_map
-        self.OPEN = []
-        self.CLOSED = {}
-    
-    def search(self, start, goal):
+        self.OPEN: List[State] = []
+        self.CLOSED: Dict[int, State] = {}
+
+    def search(self, start: State, goal: State):
         """
         Search method that needs to be implemented (either Dijkstra or A*).
         """
         raise NotImplementedError()
-            
+
+
 class Dijkstra(Search):
-            
-    def search(self, start, goal):
+    def search(self, start: State, goal: State):
         """
         Disjkstra's Algorithm: receives a start state and a goal state as input. It returns the
         cost of a path between start and goal and the number of nodes expanded.
 
         If a solution isn't found, it returns -1 for the cost.
         """
-        return -1, 0
-    
+
+        self.OPEN = []
+        self.CLOSED = {}
+
+        expand_count = 0
+
+        start.set_cost(0)
+        heapq.heappush(self.OPEN, start)
+        self.CLOSED[start.state_hash()] = start
+
+        while self.OPEN:
+            node = heapq.heappop(self.OPEN)
+
+            if node == goal:
+                return node.get_cost(), expand_count
+
+            for new_node in self.map.successors(node):
+                expand_count += 1
+
+                node_cost = node.get_cost() + self.map.cost(
+                    new_node.get_x() - node.get_x(),
+                    new_node.get_y() - node.get_y(),
+                )
+
+                if new_node.state_hash() not in self.CLOSED:
+                    new_node.set_cost(node_cost)
+                    heapq.heappush(self.OPEN, new_node)
+                    self.CLOSED[new_node.state_hash()] = new_node
+                elif (
+                    new_node.state_hash() in self.CLOSED
+                    and node_cost < new_node.get_cost()
+                ):
+                    new_node.set_cost(node_cost)
+                    heapq.heapify(self.OPEN)
+
+        return -1, expand_count
+
+
 class AStar(Search):
-    
-    def h_value(self, state):
+    def h_value(self, state: State):
         return 0
-            
-    def search(self, start, goal):
+
+    def search(self, start: State, goal: State):
         """
         A* Algorithm: receives a start state and a goal state as input. It returns the
         cost of a path between start and goal and the number of nodes expanded.
@@ -146,4 +188,3 @@ class AStar(Search):
         If a solution isn't found, it returns -1 for the cost.
         """
         return -1, 0
-    
--- Assignment1/search/map.py
+++ Assignment1/search/map.py
@@ -1,50 +1,53 @@
+from typing import List
 from search.algorithms import State
 import numpy as np
 import random
+
 
 class Map:
     """
     Class to store the map. The maps in folder dao-map are from movingai.org.
     """
-    def __init__(self, file_name):
-        self.file_name = file_name        
+
+    def __init__(self, file_name: str):
+        self.file_name = file_name
         self.map_file = open(self.file_name)
         self.type_map = self.map_file.readline()
-        self.height = int(self.map_file.readline().split(' ')[1])
-        self.width = int(self.map_file.readline().split(' ')[1])
-        
+        self.height = int(self.map_file.readline().split(" ")[1])
+        self.width = int(self.map_file.readline().split(" ")[1])
+
         State.map_width = self.width
         State.map_height = self.height
-        
+
         self.read_map()
         self.convert_data()
-        
+
         self.map_file.close()
-        
+
     def read_map(self):
         """
         Reads map from the file and stores it in memory.
         """
         line = self.map_file.readline()
-        while 'map' not in line:
+        while "map" not in line:
             line = self.map_file.readline()
         lines = self.map_file.readlines()
 
         self.data_str = []
         for line in lines:
             line_list = []
-            line = line.replace('\n', '')
+            line = line.replace("\n", "")
             for c in line:
                 line_list.append(c)
             self.data_str.append(line_list)
-        
+
     def convert_data(self):
         """
         Converts the map, initially in the movingai.org format, to a matrix of integers, where
         traversable cells have the value of 1 and non-traversable cells have the value of 0.
-        
-        The movingai.com maps are encoded as follows. 
-        
+
+        The movingai.com maps are encoded as follows.
+
         . - passable terrain
         G - passable terrain
         @ - out of bounds
@@ -57,14 +60,14 @@ class Map:
 
         for i in range(0, self.height):
             for j in range(0, self.width):
-                if self.data_str[i][j] == '.' or self.data_str[i][j] == 'G':
+                if self.data_str[i][j] == "." or self.data_str[i][j] == "G":
                     self.data_int[i][j] = 0
                 else:
-                    self.data_int[i][j] = 1        
-        
-    def random_state(self):
+                    self.data_int[i][j] = 1
+
+    def random_state(self) -> State:
         """
-        Generates a valid random state for a given map. 
+        Generates a valid random state for a given map.
         """
         x = random.randint(0, self.width - 1)
         y = random.randint(0, self.height - 1)
@@ -73,8 +76,8 @@ class Map:
             y = random.randint(0, self.height - 1)
         state = State(x, y)
         return state
-    
-    def is_valid_pair(self, x, y):
+
+    def is_valid_pair(self, x: int, y: int) -> bool:
         """
         Verifies if an x-y pair is valid for a map.
         """
@@ -85,19 +88,19 @@ class Map:
         if self.data_int[y][x] == 1:
             return False
         return True
-    
-    def cost(self, x, y):
+
+    def cost(self, x: int, y: int) -> float:
         """
         Returns the cost of an action.
-        
+
         Diagonal moves cost 1.5; each action in the 4 cardinal directions costs 1.0
         """
         if x == 0 or y == 0:
             return 1
         else:
             return 1.5
-    
-    def successors(self, state):
+
+    def successors(self, state: State) -> List[State]:
         """
         Transition function: receives a state and returns a list with the neighbors of that state in the space
         """
