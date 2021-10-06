8a616ebbac78491a32d16e8d21db59dde519d391
Michael Kwok <michael.kwok00@gmail.com>
2021-10-06 14:53:21 -0600

    Fix 2bit tb

--- Labs/Lab1/test/full_adder_2bit_tb.vhd
+++ Labs/Lab1/test/full_adder_2bit_tb.vhd
@@ -42,43 +42,44 @@ ARCHITECTURE Behavioral OF full_adder_2bit_tb IS
 BEGIN
   --Instantiate components
   adder :
-  full_adder_2bit PORT MAP(
-    a     => a_in_tb,
-    b     => b_in_tb,
-    c_in  => c_in_tb,
-    sum   => sum_out_tb,
-    c_out => c_out_tb
-  );
+  ENTITY work.full_adder_2bit(Structural)
+    PORT MAP(
+      a     => a_in_tb,
+      b     => b_in_tb,
+      c_in  => c_in_tb,
+      sum   => sum_out_tb,
+      c_out => c_out_tb
+    );
 
-  PROCESS
-    VARIABLE input_vec_tb   : UNSIGNED(4 DOWNTO 0);
-    VARIABLE expected_sum   : UNSIGNED(2 DOWNTO 0);
-    VARIABLE calculated_sum : UNSIGNED(2 DOWNTO 0);
+    PROCESS
+      VARIABLE input_vec_tb   : UNSIGNED(4 DOWNTO 0);
+      VARIABLE expected_sum   : INTEGER;
+      VARIABLE calculated_sum : UNSIGNED(2 DOWNTO 0);
 
-  BEGIN
-    FOR i IN 0 TO 31 LOOP
-      input_vec_tb := TO_UNSIGNED(i, input_vec_tb'length);
+    BEGIN
+      FOR i IN 0 TO 31 LOOP
+        input_vec_tb := TO_UNSIGNED(i, input_vec_tb'length);
 
-      expected_sum := input_vec_tb(1 DOWNTO 0) + input_vec_tb(3 DOWNTO 2) + unsigned'('0' & input_vec_tb(4));
+        expected_sum := TO_INTEGER(input_vec_tb(1 DOWNTO 0)) + TO_INTEGER(input_vec_tb(3 DOWNTO 2)) + TO_INTEGER(unsigned'("" & input_vec_tb(4)));
 
-      a_in_tb <= STD_LOGIC_VECTOR(input_vec_tb(1 DOWNTO 0));
-      b_in_tb <= STD_LOGIC_VECTOR(input_vec_tb(3 DOWNTO 2));
-      c_in_tb <= STD_LOGIC(input_vec_tb(4));
+        a_in_tb <= STD_LOGIC_VECTOR(input_vec_tb(1 DOWNTO 0));
+        b_in_tb <= STD_LOGIC_VECTOR(input_vec_tb(3 DOWNTO 2));
+        c_in_tb <= STD_LOGIC(input_vec_tb(4));
+
+        WAIT FOR clock_period_tb;
+
+        calculated_sum(2)          := c_out_tb;
+        calculated_sum(1 DOWNTO 0) := UNSIGNED(sum_out_tb);
+
+        ASSERT TO_INTEGER(calculated_sum) = expected_sum
+        REPORT "Incorrect Answer. Expected: " & INTEGER'image(expected_sum) & ". Got: " & INTEGER'image(TO_INTEGER(calculated_sum))
+        SEVERITY failure;
+
+      END LOOP;
 
       WAIT FOR clock_period_tb;
+      finish;
 
-      calculated_sum(2) := c_out_tb;
-      calculated_sum(1) := sum_out_tb;
+    END PROCESS;
 
-      ASSERT calculated_sum = expected_sum
-      REPORT "Incorrect Answer. Expected: " & INTEGER'image(TO_INTEGER(expected_sum)) & ". Got: " & INTEGER'image(TO_INTEGER(calculated_sum))
-      SEVERITY failure;
-
-    END LOOP;
-
-    WAIT FOR clock_period_tb;
-    finish;
-
-  END PROCESS;
-
-END Behavioral;
+  END Behavioral;
