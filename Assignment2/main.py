26ae16eb312c182977a0f2d5be1f1d7fd392765a
Michael Kwok <michael.kwok00@gmail.com>
2021-10-24 20:55:18 -0600

    Fix Poetry, reformat main file

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,32 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class PlotResults:
     """
-    Class to plot the results. 
+    Class to plot the results.
     """
+
     def plot_results(self, data1, data2, label1, label2, filename):
         """
         This method receives two lists of data point (data1 and data2) and plots
-        a scatter plot with the information. The lists store statistics about individual search 
+        a scatter plot with the information. The lists store statistics about individual search
         problems such as the number of nodes a search algorithm needs to expand to solve the problem.
 
-        The function assumes that data1 and data2 have the same size. 
+        The function assumes that data1 and data2 have the same size.
 
-        label1 and label2 are the labels of the axes of the scatter plot. 
-        
+        label1 and label2 are the labels of the axes of the scatter plot.
+
         filename is the name of the file in which the plot will be saved.
         """
         _, ax = plt.subplots()
-        ax.scatter(data1, data2, s=100, c="g", alpha=0.5, cmap=plt.cm.coolwarm, zorder=10)
-    
+        ax.scatter(
+            data1, data2, s=100, c="g", alpha=0.5, cmap=plt.cm.coolwarm, zorder=10
+        )
+
         lims = [
-        np.min([ax.get_xlim(), ax.get_ylim()]),  # min of both axes
-        np.max([ax.get_xlim(), ax.get_ylim()]),  # max of both axes
+            np.min([ax.get_xlim(), ax.get_ylim()]),  # min of both axes
+            np.max([ax.get_xlim(), ax.get_ylim()]),  # max of both axes
         ]
-    
-        ax.plot(lims, lims, 'k-', alpha=0.75, zorder=0)
-        ax.set_aspect('equal')
+
+        ax.plot(lims, lims, "k-", alpha=0.75, zorder=0)
+        ax.set_aspect("equal")
         ax.set_xlim(lims)
         ax.set_ylim(lims)
         plt.xlabel(label1)
@@ -34,16 +38,18 @@ class PlotResults:
         plt.grid()
         plt.savefig(filename)
 
+
 class Grid:
     """
-    Class to represent an assignment of values to the 81 variables defining a Sudoku puzzle. 
+    Class to represent an assignment of values to the 81 variables defining a Sudoku puzzle.
 
-    Variable _cells stores a matrix with 81 entries, one for each variable in the puzzle. 
+    Variable _cells stores a matrix with 81 entries, one for each variable in the puzzle.
     Each entry of the matrix stores the domain of a variable. Initially, the domains of variables
     that need to have their values assigned are 123456789; the other domains are limited to the value
-    initially assigned on the grid. Backtracking search and AC3 reduce the the domain of the variables 
+    initially assigned on the grid. Backtracking search and AC3 reduce the the domain of the variables
     as they proceed with search and inference.
     """
+
     def __init__(self):
         self._cells = []
         self._complete_domain = "123456789"
@@ -51,7 +57,7 @@ class Grid:
 
     def copy(self):
         """
-        Returns a copy of the grid. 
+        Returns a copy of the grid.
         """
         copy_grid = Grid()
         copy_grid._cells = [row.copy() for row in self._cells]
@@ -71,7 +77,7 @@ class Grid:
 
     def read_file(self, string_puzzle):
         """
-        Reads a Sudoku puzzle from string and initializes the matrix _cells. 
+        Reads a Sudoku puzzle from string and initializes the matrix _cells.
 
         This is a valid input string:
 
@@ -79,24 +85,24 @@ class Grid:
 
         This is translated into the following Sudoku grid:
 
-        - - - - - - - - - - - - - 
-        | 4 . . | . . . | 8 . 5 | 
-        | . 3 . | . . . | . . . | 
-        | . . . | 7 . . | . . . | 
-        - - - - - - - - - - - - - 
-        | . 2 . | . . . | . 6 . | 
-        | . . . | . 8 . | 4 . . | 
-        | . . . | . 1 . | . . . | 
-        - - - - - - - - - - - - - 
-        | . . . | 6 . 3 | . 7 . | 
-        | 5 . . | 2 . . | . . . | 
-        | 1 . 4 | . . . | . . . | 
-        - - - - - - - - - - - - - 
+        - - - - - - - - - - - - -
+        | 4 . . | . . . | 8 . 5 |
+        | . 3 . | . . . | . . . |
+        | . . . | 7 . . | . . . |
+        - - - - - - - - - - - - -
+        | . 2 . | . . . | . 6 . |
+        | . . . | . 8 . | 4 . . |
+        | . . . | . 1 . | . . . |
+        - - - - - - - - - - - - -
+        | . . . | 6 . 3 | . 7 . |
+        | 5 . . | 2 . . | . . . |
+        | 1 . 4 | . . . | . . . |
+        - - - - - - - - - - - - -
         """
         i = 0
         row = []
         for p in string_puzzle:
-            if p == '.':
+            if p == ".":
                 row.append(self._complete_domain)
             else:
                 row.append(p)
@@ -106,48 +112,48 @@ class Grid:
             if i % self._width == 0:
                 self._cells.append(row)
                 row = []
-            
+
     def print(self):
         """
         Prints the grid on the screen. Example:
 
-        - - - - - - - - - - - - - 
-        | 4 . . | . . . | 8 . 5 | 
-        | . 3 . | . . . | . . . | 
-        | . . . | 7 . . | . . . | 
-        - - - - - - - - - - - - - 
-        | . 2 . | . . . | . 6 . | 
-        | . . . | . 8 . | 4 . . | 
-        | . . . | . 1 . | . . . | 
-        - - - - - - - - - - - - - 
-        | . . . | 6 . 3 | . 7 . | 
-        | 5 . . | 2 . . | . . . | 
-        | 1 . 4 | . . . | . . . | 
-        - - - - - - - - - - - - - 
+        - - - - - - - - - - - - -
+        | 4 . . | . . . | 8 . 5 |
+        | . 3 . | . . . | . . . |
+        | . . . | 7 . . | . . . |
+        - - - - - - - - - - - - -
+        | . 2 . | . . . | . 6 . |
+        | . . . | . 8 . | 4 . . |
+        | . . . | . 1 . | . . . |
+        - - - - - - - - - - - - -
+        | . . . | 6 . 3 | . 7 . |
+        | 5 . . | 2 . . | . . . |
+        | 1 . 4 | . . . | . . . |
+        - - - - - - - - - - - - -
         """
         for _ in range(self._width + 4):
-            print('-', end=" ")
+            print("-", end=" ")
         print()
 
         for i in range(self._width):
 
-            print('|', end=" ")
+            print("|", end=" ")
 
             for j in range(self._width):
                 if len(self._cells[i][j]) == 1:
                     print(self._cells[i][j], end=" ")
                 elif len(self._cells[i][j]) > 1:
-                    print('.', end=" ")
+                    print(".", end=" ")
                 else:
-                    print(';', end=" ")
+                    print(";", end=" ")
 
                 if (j + 1) % 3 == 0:
-                    print('|', end=" ")
+                    print("|", end=" ")
             print()
 
             if (i + 1) % 3 == 0:
                 for _ in range(self._width + 4):
-                    print('-', end=" ")
+                    print("-", end=" ")
                 print()
         print()
 
@@ -160,7 +166,7 @@ class Grid:
 
     def is_solved(self):
         """
-        Returns True if the puzzle is solved and False otherwise. 
+        Returns True if the puzzle is solved and False otherwise.
         """
         for i in range(self._width):
             for j in range(self._width):
@@ -168,27 +174,33 @@ class Grid:
                     return False
         return True
 
+
 class VarSelector:
     """
-    Interface for selecting variables in a partial assignment. 
+    Interface for selecting variables in a partial assignment.
 
     Extend this class when implementing a new heuristic for variable selection.
     """
+
     def select_variable(self, grid):
         pass
+
 
 class FirstAvailable(VarSelector):
     """
     Naïve method for selecting variables; simply returns the first variable encountered whose domain is larger than one.
     """
+
     def select_variable(self, grid):
         # Implement here the first available heuristic
         pass
 
+
 class MRV(VarSelector):
     """
-    Implements the MRV heuristic, which returns one of the variables with smallest domain. 
+    Implements the MRV heuristic, which returns one of the variables with smallest domain.
     """
+
     def select_variable(self, grid):
         # Implement here the mrv heuristic
         pass
@@ -196,18 +208,21 @@ class MRV(VarSelector):
 
 class AC3:
     """
-    This class implements the methods needed to run AC3 on Sudoku. 
+    This class implements the methods needed to run AC3 on Sudoku.
     """
+
     def remove_domain_row(self, grid, row, column):
         """
         Given a matrix (grid) and a cell on the grid (row and column) whose domain is of size 1 (i.e., the variable has its
-        value assigned), this method removes the value of (row, column) from all variables in the same row. 
+        value assigned), this method removes the value of (row, column) from all variables in the same row.
         """
         variables_assigned = []
 
         for j in range(grid.get_width()):
             if j != column:
-                new_domain = grid.get_cells()[row][j].replace(grid.get_cells()[row][column], '')
+                new_domain = grid.get_cells()[row][j].replace(
+                    grid.get_cells()[row][column], ""
+                )
 
                 if len(new_domain) == 0:
                     return None, True
@@ -216,20 +231,22 @@ class AC3:
                     variables_assigned.append((row, j))
 
                 grid.get_cells()[row][j] = new_domain
-        
+
         return variables_assigned, False
 
     def remove_domain_column(self, grid, row, column):
         """
         Given a matrix (grid) and a cell on the grid (row and column) whose domain is of size 1 (i.e., the variable has its
-        value assigned), this method removes the value of (row, column) from all variables in the same column. 
+        value assigned), this method removes the value of (row, column) from all variables in the same column.
         """
         variables_assigned = []
 
         for j in range(grid.get_width()):
             if j != row:
-                new_domain = grid.get_cells()[j][column].replace(grid.get_cells()[row][column], '')
-                
+                new_domain = grid.get_cells()[j][column].replace(
+                    grid.get_cells()[row][column], ""
+                )
+
                 if len(new_domain) == 0:
                     return None, True
 
@@ -243,7 +260,7 @@ class AC3:
     def remove_domain_unit(self, grid, row, column):
         """
         Given a matrix (grid) and a cell on the grid (row and column) whose domain is of size 1 (i.e., the variable has its
-        value assigned), this method removes the value of (row, column) from all variables in the same unit. 
+        value assigned), this method removes the value of (row, column) from all variables in the same unit.
         """
         variables_assigned = []
 
@@ -255,7 +272,9 @@ class AC3:
                 if i == row and j == column:
                     continue
 
-                new_domain = grid.get_cells()[i][j].replace(grid.get_cells()[row][column], '')
+                new_domain = grid.get_cells()[i][j].replace(
+                    grid.get_cells()[row][column], ""
+                )
 
                 if len(new_domain) == 0:
                     return None, True
@@ -270,48 +289,49 @@ class AC3:
         """
         This method enforces arc consistency for the initial grid of the puzzle.
 
-        The method runs AC3 for the arcs involving the variables whose values are 
-        already assigned in the initial grid. 
+        The method runs AC3 for the arcs involving the variables whose values are
+        already assigned in the initial grid.
         """
         # Implement here the code for making the CSP arc consistent as a pre-processing step; this method should be called once before search
         pass
 
     def consistency(self, grid, Q):
         """
-        This is a domain-specific implementation of AC3 for Sudoku. 
+        This is a domain-specific implementation of AC3 for Sudoku.
 
-        It keeps a set of variables to be processed (Q) which is provided as input to the method. 
-        Since this is a domain-specific implementation, we don't need to maintain a graph and a set 
+        It keeps a set of variables to be processed (Q) which is provided as input to the method.
+        Since this is a domain-specific implementation, we don't need to maintain a graph and a set
         of arcs in memory. We can store in Q the cells of the grid and, when processing a cell, we
         ensure arc consistency of all variables related to this cell by removing the value of
-        cell from all variables in its column, row, and unit. 
+        cell from all variables in its column, row, and unit.
 
-        For example, if the method is used as a preprocessing step, then Q is initialized with 
+        For example, if the method is used as a preprocessing step, then Q is initialized with
         all cells that start with a number on the grid. This method ensures arc consistency by
-        removing from the domain of all variables in the row, column, and unit the values of 
-        the cells given as input. Like the general implementation of AC3, the method adds to 
-        Q all variables that have their values assigned during the propagation of the contraints. 
+        removing from the domain of all variables in the row, column, and unit the values of
+        the cells given as input. Like the general implementation of AC3, the method adds to
+        Q all variables that have their values assigned during the propagation of the contraints.
 
         The method returns True if AC3 detected that the problem can't be solved with the current
-        partial assignment; the method returns False otherwise. 
+        partial assignment; the method returns False otherwise.
         """
         # Implement here the domain-dependent version of AC3.
         pass
 
+
 class Backtracking:
     """
-    Class that implements backtracking search for solving CSPs. 
+    Class that implements backtracking search for solving CSPs.
     """
 
     def search(self, grid, var_selector):
         """
-        Implements backtracking search with inference. 
+        Implements backtracking search with inference.
         """
         # Implemente here the Backtracking search.
         pass
 
 
-file = open('tutorial_problem.txt', 'r')
+file = open("tutorial_problem.txt", "r")
 # file = open('top95.txt', 'r')
 problems = file.readlines()
 
@@ -321,11 +341,11 @@ for p in problems:
     g.read_file(p)
 
     # Print the grid on the screen
-    print('Puzzle')
+    print("Puzzle")
     g.print()
 
     # Print the domains of all variables
-    print('Domains of Variables')
+    print("Domains of Variables")
     g.print_domains()
     print()
 
@@ -333,33 +353,33 @@ for p in problems:
     for i in range(g.get_width()):
         for j in range(g.get_width()):
 
-            print('Domain of ', i, j, ': ', g.get_cells()[i][j])
+            print("Domain of ", i, j, ": ", g.get_cells()[i][j])
 
             for d in g.get_cells()[i][j]:
-                print(d, end=' ')
+                print(d, end=" ")
             print()
 
     # Make a copy of a grid
     copy_g = g.copy()
 
-    print('Copy (copy_g): ')
+    print("Copy (copy_g): ")
     copy_g.print()
     print()
 
-    print('Original (g): ')
+    print("Original (g): ")
     g.print()
     print()
 
     # Removing 2 from the domain of the variable in the first row and second column
-    copy_g.get_cells()[0][1] = copy_g.get_cells()[0][1].replace('2', '')
+    copy_g.get_cells()[0][1] = copy_g.get_cells()[0][1].replace("2", "")
 
     # The domain (0, 1) of copy_g shouldn't have 2 (first list, second element)
-    print('copy_g')
+    print("copy_g")
     copy_g.print_domains()
     print()
 
     # The domain of variable g shouldn't have changed though
-    print('g')
+    print("g")
     g.print_domains()
     print()
 
@@ -370,21 +390,21 @@ for p in problems:
     variables_assigned, failure = ac3.remove_domain_row(g, 0, 0)
 
     # The domain of all variables in the first row must not have 4
-    print('Removed all 4s from the first row')
+    print("Removed all 4s from the first row")
     g.print_domains()
 
     # variables_assigned contains all variables whose domain reduced to size 1 in the remove_domain_row opeation
-    print('Variables that were assigned by remove_domain_row: ', variables_assigned)
+    print("Variables that were assigned by remove_domain_row: ", variables_assigned)
 
     # failture returns True if any of the variables in the row were reduced to size 0
-    print('Failure: ', failure)
+    print("Failure: ", failure)
     print()
 
     # Making all variables in the first column arc consistent with (0, 0), whose value is 4
     variables_assigned, failure = ac3.remove_domain_column(g, 0, 0)
 
     # The domain of all variables in the first column must not have 4
-    print('Removed all 4s from the first column')
+    print("Removed all 4s from the first column")
     g.print_domains()
     print()
 
@@ -392,8 +412,6 @@ for p in problems:
     variables_assigned, failure = ac3.remove_domain_unit(g, 0, 0)
 
     # The domain of all variables in the first column must not have 4
-    print('Removed all 4s from the first unit')
+    print("Removed all 4s from the first unit")
     g.print_domains()
     print()
-
-
