ee818383b26cd0825c640ccf152b50e1d6dd36d2
Michael Kwok <michael.kwok00@gmail.com>
2021-11-22 18:15:39 -0700

    Implement Program 2

--- Labs/Lab3/src/ProgMem.vhd
+++ Labs/Lab3/src/ProgMem.vhd
@@ -11,8 +11,8 @@ END ENTITY;
 ARCHITECTURE rtl OF ProgMem IS
   TYPE rom_type IS ARRAY (0 TO 31) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
   SIGNAL ROM : rom_type := (
-    "10000001",            -- IN A (Lower)
-    "10000000",            -- IN A (Upper)
+    "10000001",            -- IN A (Upper)
+    "10000000",            -- IN A (lower)
     "11000000",            -- JZ
     "00000101",            -- 05
     "01100010",            -- SHFL A, 2
@@ -23,18 +23,13 @@ ARCHITECTURE rtl OF ProgMem IS
     "10010000",            -- OUT A
     OTHERS => "10100000"); -- HALT
   -- SIGNAL ROM : rom_type := (
-  --   "10000000",            -- IN A (Upper)
-  --   "10000001",            -- IN A (Lower)
-  --   "00100000",            -- STA 0, A
+  --   "10000001",            -- IN A (Upper)
+  --   "10000000",            -- IN A (Lower)
+  --   "00100100",            -- STA R[4], A
   --   "00110000",            -- LDI A,
-  --   "00101000",            -- 40
-  --   "01000000",            -- ADD A, 0
-  --   "00100001",            -- STA 1, A
-  --   "00110000",            -- LDI A,
-  --   "01100100",            -- 100
-  --   "01010001",            -- SUB A, 1
-  --   "01100001",            -- SHFL A, 1
-  --   "01110010",            -- SHFR A, 2
+  --   "00001111",            -- 15
+  --   "01000100",            -- ADD A, R[4]
+  --   "10010000",            -- OUT A
   --   OTHERS => "10100000"); -- HALT
 BEGIN
   PROCESS (pm_addr)
