6f9c2c9e3e2b62884d8866cb190855d6b5205c5c
Michael Kwok <michael.kwok00@gmail.com>
2022-03-06 23:52:11 -0700

    Finish Question 1

---
+++ Assignment3/1548454.pl
@@ -0,0 +1,46 @@
+% Spec: a parameter headed with + means it is an input parameter--it's bound when the predicate is called, and a parameter headed with - is an output parameter--it's a variable when the predicate is called and bound to a value when the predicate is proved. A parameter headed by ? means it can be either input or output parameter. 
+
+% Question 1
+% setDifference(+S1,+S2,-S3)
+% S1 and S2 are lists of atoms, S3 is list of atoms that are in S1 but not in S2. Assume S1 is a set.
+% Ex:
+% setDifference([a,b,c,d,e,g], [b,a,c,e,f,q], S).
+% S=[d,g]
+setDifference(LFirst, LSecond, LOut) :- setDifference(LFirst, LSecond, [], LOut).
+
+setDifference([], _, Out, Out).
+
+setDifference([Head|Tail], LSecond, Acc, LOut) :-
+    (memberchk(Head, LSecond) ->
+    setDifference(Tail, LSecond, Acc, LOut);
+    setDifference(Tail, LSecond, [Head|Acc], LOut)).
+
+
+% Question 2
+% swap(+L, -R)
+% L is list of atoms, R is list of atoms from L swapped
+% Ex:
+% swap([a,1,b,2], W).
+% W = [1,a,2,b].
+% swap([a,1,b], W).
+% W = [1,a,b].
+
+% Question 3
+% filter(+L,+OP,+N,-L1)
+% L is possibly nested list of numbers
+% OP is either equal, greaterThan or lessThan
+% N is a number
+% L1 is a flat list of all numbers in L that satisfies condition formed by OP and N
+% Ex:
+% filter([3,4,[5,2],[1,7,3]],greaterThan,3,W).
+% W= [4,5,7]
+% filter([3,4,[5,2],[1,7,3]],equal,3,W).
+% W= [3,3]
+% filter([3,4,[5,2],[1,7,3]],lessThan,3,W).
+% W= [2,1]
+
+filter(LIn, equal, N, LOut).
+
+filter(LIn, greaterThan, N, LOut).
+
+filter(LIn, lessThan, N, LOut).
